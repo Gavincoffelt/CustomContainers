bbdf984106e5fc8004c248614b83c27b06d39878
Gavin Coffelt <42452113+Gavincoffelt@users.noreply.github.com>
2018-11-23 20:54:10 -0800

    2nd Push
    Added Overloaded = & [], Resize, Reserve, Pushback, Size, Capacity and Empty

--- CustomContainers/CustomContainers/Main.cpp
+++ CustomContainers/CustomContainers/Main.cpp
@@ -1,20 +1,13 @@
 #include <iostream>
-#include <vector>
+#include "vector.h"
 
 using namespace std;
 int main() {
-	vector <int> intvector;
-	for (int i = 0; i < 10; i++) {
-		intvector.push_back(i);
-	}
-	cout << intvector.size() << endl;
-	cout << "Size:" << intvector.size() << endl;
-	for (int i = 0; i < intvector.size(); i++) {
-		cout << "Vector Contains:" << intvector.at(i) << endl;
-	}
-
-		
-
+	vectorclass<int> bob;
+	bob.resize(10);
+	
+	cout << bob.size() << endl;
+	
 	
 	return 0;
 }
--- CustomContainers/CustomContainers/vector.h
+++ CustomContainers/CustomContainers/vector.h
@@ -11,49 +11,95 @@ public:
 
 	~vectorclass()  
 	{
-		
+		delete[] vector_pointer;
 	}
 
 	vectorclass& operator =(vectorclass other) 
-	
+	{
+		swap(*this, other);
+		return *this;
+	}
 
 
 	void resize(const size_t &newsize) 
-	
+	{
+		if (newsize < vector_size)
+		{
+			for (size_t i = newsize; i < vector_size; i++)
+			{
+				vector_pointer[i].~T();
+			}
+		}
+		else if (newsize > vector_capacity)
+		{
+			reserve(newsize);
+		}
+		vector_size = newsize;
+	}
 
 	void reserve(size_t newcapacity) 
-	
+	{
+		
+		if (newcapacity > vector_capacity)
+		{
+			T* temp = new T[newcapacity];
+			std::copy(vector_pointer, vector_pointer + vector_capacity, temp);
+			delete[] vector_pointer;
+			vector_pointer = temp;
+			vector_capacity = newcapacity;
+		}
+	}
 
 	void push_back(const T &val)
-
+	{
+		if (vector_capacity <= vector_size)
+			reserve(vector_capacity);
+		vector_pointer[vector_size++] = val;
+	}
 
 	void pop_back() 
-	
+	{
+		
+	}
 	void insert(const size_t& pos, const T& val)
-
+	{
+		
+	}
 
 	size_t size() const
-	
-	size_t capacity() const
-	
-	T& operator [](const size_t &pos)
-	
+	{
+		return vector_size;
+	}
 
-	const T& operator [](const size_t &pos) const
-	
+	size_t capacity() const
+	{
+		return vector_capacity;
+	}
+
+	T& operator [](const size_t &pos)
+	{
+		return vector_pointer[pos];
+	}
+
+
 	bool empty() const
-	
+	{
+		return vector_size == 0;
+	}
 
 	T& at(const size_t& pos)  
-	
+	{
+		
+	}
 
-	const T& at(const size_t& pos) const
-	
 
 	void erase(const size_t& pos) 
-
+	{
+		
+	}
 
 	void clear()
-	
+	{
 
+	}
 };
